3a80890ae868969dda262d2166854b061dbddeb6
er-md-ejaz <180041671+er-md-ejaz@users.noreply.github.com>
2025-07-07 22:01:25 +0530

    remove semicolon

--- Lecture_15/adaptive_thresholding.py
+++ Lecture_15/adaptive_thresholding.py
@@ -3,8 +3,8 @@ import numpy as np
 
 img = cv.imread('sudoku.png',0)
 _, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
-th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2);
-th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2);
+th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
 
 cv.imshow("Image", img)
 cv.imshow("THRESH_BINARY", th1)
